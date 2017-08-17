4c81583f4d47ffef5a974217f4c8e151e8e94b47
ai-coder <akashkumar0216@gmail.com>
2017-08-17 19:51:12 +0530

    First version of Linear Regression is complete

--- algorithms/linear_reg/plotdata.py
+++ algorithms/linear_reg/plotdata.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from mpl_toolkits.mplot3d import axes3d
 from sklearn.linear_model import LinearRegression
+from . import linearreg as lr
 
 """The following function plots X and y on both the axis. plt.scatter plots the data, s gives the size of the marker, c gives the color of the marker and marker='x' gives the pointer style. We have passed strings as x_label and y_label"""
 
@@ -17,12 +19,14 @@ def plotDataJ(x_label, y_label, J):
 	plt.xlabel(x_label)
 	plt.ylabel(y_label)
 	plt.show()
-	
+
+"""plotting the hypothesis with calulated theta(optimised)"""	
 def plotDataH(X,y,x_label, y_label, hx, range_x):
 	plt.scatter(X[:,1], y, s=30, c='r', marker='x', linewidths=1)
 	plt.plot(range_x, hx)
 	plt.show()
 	
+"""plotting the hypothesis calculated using Scikit-Learn"""
 def plotDataSK(X, y, range_x, hx):
 	plt.scatter(X[:,1], y, s=30, c='r', marker='x', linewidths=1)
 	plt.plot(range_x, hx, label='Linear Regression with simple gradient descent')
@@ -30,4 +34,19 @@ def plotDataSK(X, y, range_x, hx):
 	regr.fit(X[:,1].reshape(-1,1), y.ravel())
 	plt.plot(range_x, regr.intercept_+regr.coef_*range_x, label='Linear Regression with Scikit Learn')
 	plt.legend(loc=4)
+	plt.show()
+
+"""plotting cost for some 2 dimensional domain. linspace defines the domain for which we have to plot i.e. x-axis and y-axis of the graph."""	
+def plotDataCOST(X, y):
+	theta_0 = np.linspace(-10,10,50)
+	theta_1 = np.linspace(-1,4,50)
+	J_temp = np.zeros((theta_0.size,theta_1.size))
+	for i in range(50):
+		for j in range(50):
+			theta=[[theta_0[i]],[theta_1[j]]]
+			J_temp[i,j]=lr.ComputeCost(X, y, theta)
+	#This is the new way of plotting 3d graphs in matplotlib
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection='3d')
+	ax.plot_surface(theta_0, theta_1, J_temp, rstride=1, cstride=1, alpha=1, cmap=plt.cm.jet)
 	plt.show()
--- driver.py
+++ driver.py
@@ -23,6 +23,7 @@ plt.close()
 #ComputeCost computes the cost of the linear regression
 theta =[[0],[0]]
 J = lr.ComputeCost(X, y, theta)
+print('The cost of the hypothesis created with theta as 0,0 : ')
 print(J)
 
 iteration = 1500
@@ -35,10 +36,12 @@ theta, J_plot= lr.gradientDescent(X, y, theta, iteration, alpha)
 #plotting the cost of hypothesis in 1500 iterations
 pd.plotDataJ('x-label', 'y-label', J_plot)
 #the optimised theta is
+print('The optimised theta is : ')
 print(theta)
 
 #new cost must decrease
 J = lr.ComputeCost(X,y,theta)
+print('The new cost after the optimised theta is : ')
 print(J)
 
 #plotting the predicted function i.e. theta0*x0 + theta1*x1
@@ -51,15 +54,12 @@ pd.plotDataH(X, y, 'x-label', 'y-label', hx, range_x)
 #using scikit-learn to plot the same and comapring both the values
 pd.plotDataSK(X, y, range_x, hx)
 
+#predicting profits - theta(T).x
+print(' The predicted value for custom x is : ')
+print(theta.T.dot([1, 3.5])*10000)
+
+#plotting cost for each value of theta and plotting a 3D graph of the same
+pd.plotDataCOST(X, y)
 
 
-
-
-
-
-
-
-
-
-
-
+""" Contour plots are still to be completed"""
