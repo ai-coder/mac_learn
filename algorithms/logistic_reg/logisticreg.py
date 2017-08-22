08e7c764f84a5a3850ea18b44b8f2e76f75aed92
ai-coder <akashkumar0216@gmail.com>
2017-08-22 17:03:31 +0530

    the minimize function need to be corrected

--- algorithms/logistic_reg/logisticreg.py
+++ algorithms/logistic_reg/logisticreg.py
@@ -6,6 +6,26 @@ def sigmoid(Z):
 	return 1/(1+np.exp(-Z))
 	
 """computing cost for a given X and y in logistic regression i.e. binary classification"""
-def computeCost(X, y, theta):
+def computeCost(theta, X, y):
 	J = (-1/y.size) * (np.log(sigmoid(X.dot(theta))).T.dot(y) + (np.log(1-sigmoid(X.dot(theta))).T.dot(1-y)))
-	return(J, 0)
+	if np.isnan(J[0]):
+		return(np.inf)
+	grad = (1/y.size)*X.T.dot(sigmoid(X.dot(theta.reshape(-1,1))) - y)
+	return(J, grad.flatten())
+	#flatten converts the two dimentional array i.e. [[x],[y],[z]] into one dimensional i.e. [x, y, z]
+	
+"""To use the minimize function we need to code the function returning grad and theta separately. The following function is trying to implement cost function and grad descent separately so that the it is compatible with the minimize function in the driver1.py file"""
+
+def cGrad(theta, X, y):
+	m = y.size
+	h = sigmoid(X.dot(theta.reshape(-1,1)))
+	grad = (1/m)*X.T.dot(h-y)
+	return(grad.flatten())
+	
+def cCost(theta, X,y):
+	m = y.size
+	h = sigmoid(X.dot(theta))
+	J=-1*(1/m)*(np.log(h).T.dot(y)+np.log(1-h).T.dot(1-y))
+	if np.isnan(J[0]):
+		return(np.inf)
+	return(J[0])
--- driver1.py
+++ driver1.py
@@ -1,6 +1,7 @@
 import numpy as np
 import algorithms.logistic_reg.logisticreg as lr
 import algorithms.logistic_reg.plotdata as pd
+from scipy.optimize import minimize
 
 #importing data which is 100x3  || to get the dimensions of a matrix we use data.shape
 data=np.genfromtxt('datas/data2_1.txt', delimiter=',')
@@ -13,8 +14,14 @@ y = np.c_[data[:,2]]
 #plotting the data
 pd.plotData(data ,X, y, 'x-label', 'y-label', 'positive', 'negative')
 
-#calculating cost at theta as 0s
-theta = [[0],[0],[0]]
-J, grad = lr.computeCost(X, y, theta)
+#calculating cost at theta as 0s and gradient
+#theta = [[0],[0],[0]]
+theta = np.zeros(X.shape[1])
+J, grad = lr.computeCost(theta, X, y)
 print('The cost of the given data with theta as 0s is : ')
 print(J)
+print('The gradient is : ')
+print(grad)
+
+"""Improve the fucntions in the algorithm folder in order to make this function comaptible with them"""
+#res = minimize(lr.cCost, theta, args=(X,y), method=None, jac=lr.cGrad, options={'maxiter':400})
